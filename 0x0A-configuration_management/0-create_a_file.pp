4664292814fdf01b81d20b4ebcd27453b5e61aad
tunde <blackkid306@gmail.com>
2023-04-23 15:16:28 -0700

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-# creates a filei
-file    {'/tmp/school':
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love puppet'
+# manifest to create file in tmp directory
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
