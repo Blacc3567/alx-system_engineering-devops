1af7e7396f9507a03ef833974464edc4ce21261d
tunde <blackkid306@gmail.com>
2023-04-23 14:58:51 -0700

    o

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # creates a filei
-file ('/tmp/school':
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+file    ('/tmp/school':
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love puppet'
 )
